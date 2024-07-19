efa7e76a9fdf9e4a6bf44ce9f7338cd20933d9b1
Diana Campos <dcampos0495@gmail.com>
2024-07-18 18:54:04 -0600

    create user collection con auth data

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from 'firebase/auth'
 import Constants from 'expo-constants'
 
 const firebaseConfig = {
@@ -14,3 +15,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const database = getFirestore(app);
+export const firebase_auth = getAuth(app)
