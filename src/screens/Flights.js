e440f0ba0920d3b5ca59df78d40361a2d561f7be
Diana Campos <dcampos0495@gmail.com>
2024-07-25 19:13:49 -0600

    Get user name data to Home Screen

--- src/screens/Flights.js
+++ src/screens/Flights.js
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View, FlatList, Pressable,} from 'react-native'
 import React, {useEffect, useState} from 'react'
 import { database, firebase_auth as auth} from '../config/firebase';
-import {querySnapshot, collection, onSnapshot, orderBy, query, where} from 'firebase/firestore'
+import {querySnapshot, collection, onSnapshot, orderBy, query, where, doc, getDoc} from 'firebase/firestore'
 import { signOut } from 'firebase/auth';
 import { Ionicons } from '@expo/vector-icons';
 import FlightCard from '../components/FlightCard';
@@ -10,7 +10,7 @@ import Loader from '../components/Loader';
 
 const Flights = ({navigation}) => {
   const userId = auth.currentUser?.uid || '';
-
+  const [user, setUser] = useState([])
   const [flights, setFlights] = useState([])
   const [loading, setLoading] = useState(true)
 
@@ -18,7 +18,6 @@ const Flights = ({navigation}) => {
     if(userId) {
       const collectionRef = collection(database, 'flights')
       const q = query(collectionRef, where('userId', '==', userId), orderBy('createdAt', 'asc'))
-
       const unsuscribe = onSnapshot(q, querySnapshot => {
         setFlights(
           querySnapshot.docs.map(doc => ({
@@ -31,6 +30,7 @@ const Flights = ({navigation}) => {
           })
         )
       )
+      getUser(userId)
       setLoading(false)
       })
       return unsuscribe
@@ -38,6 +38,18 @@ const Flights = ({navigation}) => {
   setLoading(false)
 }
   }, [userId])
+
+  const getUser = async (userId) => {
+    try {
+      const docRef = doc(database, 'users', userId);
+      const docSnap = await getDoc(docRef);
+      if(docSnap.exists()) {
+        setUser(docSnap.data())
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   const signOutWithFirebase = async () => {
     setLoading(true)
@@ -61,9 +73,12 @@ const Flights = ({navigation}) => {
     <View style={styles.container}>
       <View style={styles.header_container}>
         <Text style={styles.title}>My Flights</Text>
-        <Pressable onPress={signOutWithFirebase}>
-          <Text style={styles.logout}>log out</Text>
-        </Pressable>
+        <View style={styles.name_container}>
+          <Text style={styles.name_user}>{user.name} {user.lastname}</Text>
+          <Pressable onPress={signOutWithFirebase}>
+            <Text style={styles.logout}>log out</Text>
+          </Pressable>
+        </View>
       </View>
       {flights == 0
         ? <Text style={styles.title_empty}>You haven't booked any flights yet</Text>
@@ -98,13 +113,19 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between'
   },
+  name_container: {
+    paddingRight: 5,
+  },
+  name_user: {
+    fontSize: 17,
+    color: '#725099',
+    fontWeight: '500',
+  },
   logout: {
     textAlign: 'center',
-    paddingRight: 10,
-    paddingTop: 15,
+    paddingTop: 3,
     fontSize: 17,
     fontWeight: 'bold',
-    textAlignVertical: 'center',
     color: '#3441bd',
   },
   title: {
